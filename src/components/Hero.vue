b5371f359ad4bb8acd2db208bae075e44fdad573
Modestas Skirius <modestas.skirius@gmail.com>
2022-04-13 20:31:16 +0300

    add logos section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-    <section>
+    <section class="first-section">
         <header>
             <h1>Slack is your digital HQ</h1>
             <p>Transform the way you work with one place for everyone and everything you need to get stuff done.</p>
@@ -8,7 +8,48 @@
                 <button class="btn-google">Sign up with google</button>
             </div>
         </header>
-        <img class="img-hero" src="./../assets/img-campaign-hero@2x.jpg" alt="chatExample">
+        <div class="img-wrapper">
+            <img class="img-hero" src="./../assets/img-campaign-hero@2x.jpg" alt="chatExample">
+        </div>
+    </section>
+    <section class="second-section">
+        <div class="logos">
+            <div class="logo-wrapper">
+                <a href="_blank">
+                    <img src="../assets/fox@2x.png" alt="fox">
+                </a>
+            </div>
+            <div class="logo-wrapper">
+                <a href="_blank">
+                    <img src="../assets/lonelyplanet@2x.png" alt="flonelyplanet">
+                </a>
+            </div>
+            <div class="logo-wrapper">
+                <a href="_blank">
+                    <img src="../assets/intuit@2x.png" alt="intuit">
+                </a>
+            </div>
+            <div class="logo-wrapper">
+                <a href="_blank">
+                    <img src="../assets/carvana@2x.png" alt="carvana">
+                </a>
+            </div>
+            <div class="logo-wrapper">
+                <a href="_blank">
+                    <img src="../assets/kiva@2x.png" alt="kiva">
+                </a>
+            </div>
+            <div class="logo-wrapper">
+                <a href="_blank">
+                    <img src="../assets/target-logo@2x.png" alt="target">
+                </a>
+            </div>
+            <div class="logo-wrapper">
+                <a href="_blank">
+                    <img src="../assets/devacurl@2x.png" alt="devacurl">
+                </a>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -19,8 +60,8 @@ export default {
 </script>
 
 <style>
-    section {
-        display: flex;
+    .first-section {
+        display: grid;
         flex-direction: column;
         background: #f6efe8;
         color: black;
@@ -29,6 +70,7 @@ export default {
     header {
         display: flex;
         flex-direction: column;
+        width: 100%;
     }
     h1 {
         font-weight: 700;
@@ -67,22 +109,48 @@ export default {
         padding: 20px 40px;
     }
     .img-hero {
+        width: 100%;
+        height: auto;
+        margin-top: 16px;
+    }
+    .second-section {
+        background: #f6efe8;
+        padding: 2rem;
+    }
+    .logos {
         display: flex;
         width: 100%;
-        height: 100%;
-        margin-top: 16px;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-items: center;
+    }
+    .logo-wrapper {
+        max-width: 170px;
+        margin: 10px;
+        text-align: center;
+    }
+    img {
+        width: 100%;
     }
     
     @media (min-width: 768px) {
         section {
+            grid-template-columns: 1fr 1fr;
+            gap: 16px;
+        }
+        .img-hero {
+            margin-top: 0px;
+        }
+    @media (min-width: 1024px) {
+        h1 {
+            font-size: 4rem;
+            line-height: 1.1;
+        }
+    }
+    @media (min-width: 1173px) {
+        .buttons {
             flex-direction: row;
         }
-        header {
-            flex: 1;
-        }
-        img-hero {
-            flex: 1;
-        }
-
+    }
 }
 </style>
